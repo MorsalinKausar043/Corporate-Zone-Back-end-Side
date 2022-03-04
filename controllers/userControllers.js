fd8fc7dffdbe9991dfef371a442e113d257fc5db
coderyeasin <yeasin3233@gmail.com>
2022-03-04 15:28:27 +0600

    done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -34,21 +34,8 @@ const postUser = async (req, res) => {
   }
 };
 
-// crete review
-
-// const review = async (req,res) =>{
-//   try {
-//     const newReview = new newReview(req.body);
-//     const result = await newReview.save();
-//     res.json(result);
-
-//   } catch (err) {
-//     res.status(500).json({message:err.message})
-//   }
-// }
-
-// get all users
-const allUsers = async (req, res) => {
+// update a user
+const updateUser = async (req, res) => {
   try {
     const email = req.params.email;
     const query = { email: email };
@@ -62,29 +49,4 @@ const allUsers = async (req, res) => {
   }
 };
 
-// get user by id
-const getUserById = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const query = { _id: mongoose.Types.ObjectId(id) };
-    const result = await User.findOne({ query: query });
-    res.json(result);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-// delete users
-const deleteUserById = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const query = { _id: mongoose.Types.ObjectId(id) };
-    const result = await User.findOneAndDelete(query);
-    console.log(result);
-    res.json(result);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-module.exports = { postUser, allUsers, getUserById, deleteUserById };
+module.exports = { postUser, allUsers, getUser, updateUser };
